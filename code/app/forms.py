23e23f48c9df15eddbdb7835ca32fffbe90dff03
dkkapur <dkkapur@users.noreply.github.com>
2016-04-18 23:24:21 -0400

    added new elmts to signup

--- code/app/forms.py
+++ code/app/forms.py
@@ -19,6 +19,22 @@ class SignupForm(Form):
     gender = TextField('gender', validators=[Required()])
     age = TextField('age', validators=[Required()])
     location = TextField('location', validators=[Required()])
+    extraverted = TextField('extraverted', validators=[Required()])
+    agreeable = TextField('agreeable', validators=[Required()])
+    consc = TextField('consc', validators=[Required()])
+    emotional = TextField('emotional', validators=[Required()])
+    experiences = TextField('experiences', validators=[Required()])
+    pleasureseeking = TextField('pleasureseeking', validators=[Required()])
+    nostalgia = TextField('nostalgia', validators=[Required()])
+    catharsis = TextField('catharsis', validators=[Required()])
+    aggression = TextField('aggression', validators=[Required()])
+    escapism = TextField('escapism', validators=[Required()])
+    sensationseeking = TextField('sensationseeking', validators=[Required()])
+    artistic = TextField('artistic', validators=[Required()])
+    informationseeking = TextField('informationseeking', validators=[Required()])
+    boredomavoidance = TextField('boredomavoidance', validators=[Required()])
+    socialization = TextField('socialization', validators=[Required()])
+
 
 class ReviewForm(Form):
     name = TextField('name', validators=[Required()])
